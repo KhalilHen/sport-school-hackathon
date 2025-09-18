e16d236e77e134744e58472cdc007cf5ac7a28db
nick <nickswanenberg@gmail.com>
2025-09-18 14:41:07 +0200

    Added prop data in a component

--- src/pages/CoachPage/coachPage.jsx
+++ src/pages/CoachPage/coachPage.jsx
@@ -1,12 +1,31 @@
 import "../CoachPage/coachPage.scss";
+import DataListPage from "../../components/DataListComponent/DataListComponent";
 
 export default function CoachPage() {
   return (
     <div className="container">
       <section className="hero">
         <div className="coachPageContainer">
-          <h1>Meet Our Coaches</h1>
-          <div className="Coaches">
+          <DataListPage
+            title="Our Coaches"
+            fetchUrl="http://localhost:5149/api/Coach"
+            renderItem={(coach) => (
+              <div className="coachCard">
+                <div className="coachImage">
+                  <img src="src/assets/sportcoach.jpg" alt="Coach 1" />
+                </div>
+                <div className="coachSpecialization">
+                  <p>FitCoach{coach.specialization}</p>
+                </div>
+                <div className="coachInfo">
+                  <p>By {coach.name}</p>
+                  <p>€35{coach.price}</p>
+                </div>
+              </div>
+            )}
+          />
+          {/* <h1> Meet Our Coaches</h1>
+          <div className="Coaches"> 
             <div className="coachCard">
               <div className="coachImage"><img src="src/assets/sportcoach.jpg" alt="Coach 1" /></div>
               <div className="coachSpecialization">
@@ -87,9 +106,9 @@ export default function CoachPage() {
                 <p>€35</p>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
-      </section>
-    </div>
+      </section >
+    </div >
   );
 }
